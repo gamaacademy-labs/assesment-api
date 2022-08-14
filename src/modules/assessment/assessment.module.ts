9d9f4f97fc2ad8a10f820fe346415ab1471a06f4
Alvaro Ferreira <alvaromachadoferreira@hotmail.com>
2022-08-14 12:42:52 -0300

    feat: add module

---
+++ src/modules/assessment/assessment.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AssessmentController } from './assessment.controller';
+import { AssessmentRepository } from './assessment.repository';
+import { AssessmentService } from './assessment.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([AssessmentRepository])
+  ],
+  controllers: [AssessmentController],
+  providers: [AssessmentService]
+})
+export class AssessmentModule {}
