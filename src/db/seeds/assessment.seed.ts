c905d62d2bba5fb29a662947f18b541da4b64f0e
Alvaro Ferreira <alvaromachadoferreira@hotmail.com>
2022-08-16 12:05:11 -0300

    feat: add assessment seeds

---
+++ src/db/seeds/assessment.seed.ts
@@ -0,0 +1,16 @@
+import { Connection } from 'typeorm';
+import { Factory, Seeder } from 'typeorm-seeding';
+import { AssessmentEntity } from '../../modules/assessment/assessment.entity';
+
+export default class Assessment implements Seeder {
+  public async run(factory: Factory, connection: Connection): Promise<void> {
+    await connection
+      .createQueryBuilder()
+      .insert()
+      .into(AssessmentEntity)
+      .values([
+        { title: 'Avaliação de Estrutura de Dados', finishedAt: '2020-01-01', questions: [{  }] },
+      ])
+      .execute();
+  }
+}
