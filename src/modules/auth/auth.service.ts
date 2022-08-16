35bd76f4920496d3b919da2fabb2a09470edf052
Alvaro Ferreira <alvaromachadoferreira@hotmail.com>
2022-08-16 11:22:13 -0300

    feat: add validateUserCredentials && loginWithCredentials

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,12 +1,32 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 import { AuthDto } from './auth.dto';
 
 @Injectable()
 export class AuthService {
     constructor(
-        private usersService: UsersService
+        private usersService: UsersService,
+        private jwtTokenService: JwtService
     ) { }
+
+    async validateUserCredentials(username: string): Promise<any> {
+        const user = await this.usersService.findUserByUsername(username);
+
+        if(user && user.username === username) {
+        const {username, ...result} = user;
+        return result;
+        }
+        return null;
+    }
+
+    async loginWithCredentials(user: any) {
+        const payload = { username: user.username, sub: user.id };
+
+        return {
+            access_token: this.jwtTokenService.sign(payload),
+        };
+    }
 
     public async login(payload: AuthDto): Promise<string> {
         const auth = await this.usersService.findUserByUsername(payload.username)
