6275e6570accd60ed85cfd9fa4690b943a8e4ea7
Alvaro Ferreira <alvaromachadoferreira@hotmail.com>
2022-08-16 11:27:27 -0300

    feat: update login route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request ,UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
+import { AuthService } from './modules/auth/auth.service';
+import { JwtAuthGuard } from './modules/auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService, 
+    private authService: AuthService
+    ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.loginWithCredentials(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('user-info')
+  getUserInfo(@Request() req) {
+    return req.user
   }
 }
