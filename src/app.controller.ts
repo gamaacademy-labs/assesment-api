32709aeee9af86272613c0d7b14f9b442d4784de
sampaiorafael <rafasampaio021@hotmail.com>
2022-08-17 10:45:12 -0300

    refactor: remove endpoints from app.controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,30 +1,7 @@
-import { Controller, Get, Post, Request ,UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { AppService } from './app.service';
-import { AuthService } from './modules/auth/auth.service';
-import { JwtAuthGuard } from './modules/auth/jwt-auth.guard';
+import { Controller } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService, 
-    private authService: AuthService
-    ) {}
+  constructor() {}
 
-  // @Get()
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
-
-  @UseGuards(AuthGuard('local'))
-  @Post('login')
-  async login(@Request() req) {
-    return this.authService.loginWithCredentials(req.user);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('user-info')
-  getUserInfo(@Request() req) {
-    return req.user
-  }
 }
