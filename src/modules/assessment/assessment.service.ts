c85ec3bd4e7009651a7f5bf7fbcdf0b5ca35bba7
Alvaro Ferreira <alvaromachadoferreira@hotmail.com>
2022-08-18 17:16:19 -0300

    feat: add assessment service

---
+++ src/modules/assessment/assessment.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AssessmentService {
+    findAssessmentById(id: number) {
+        throw new Error('Method not implemented.');
+    }
+}
