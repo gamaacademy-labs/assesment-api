a2fb9629eb4a0389eb36452fbeed8e60939c608d
sampaiorafael <rafasampaio021@hotmail.com>
2022-08-18 10:40:20 -0300

    feat: deploy configs

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -14,8 +14,9 @@ export const typeormConfig = (): TypeOrmModule => {
         synchronize: false,
         logging: process.env.NODE_ENV === 'development' ? true : false,
         autoLoadEntities: true,
-        extra: {
-          ssl: true
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
         }
       }
 }
