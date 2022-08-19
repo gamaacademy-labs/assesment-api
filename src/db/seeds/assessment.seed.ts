448c345d08d03ebd9b76697a8776327dfd93b27f
GeFFz <gefferson_oliveira@outlook.com>
2022-08-19 16:15:55 -0300

    refactor: force uuid seed assessment

--- src/db/seeds/assessment.seed.ts
+++ src/db/seeds/assessment.seed.ts
@@ -9,7 +9,12 @@ export default class Assessment implements Seeder {
       .insert()
       .into(AssessmentEntity)
       .values([
-        { title: 'Avaliação de Estrutura de Dados', finishedAt: '2020-01-01', questions: [] },
+        { 
+          id: '1aca16af-36a3-4850-9aef-eadb6f8a8f30',
+          title: 'Avaliação de Estrutura de Dados', 
+          finishedAt: '2020-01-01', 
+          questions: [],
+        },
       ])
       .execute();
   }
