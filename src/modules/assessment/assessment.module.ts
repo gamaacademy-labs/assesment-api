66cdc0108c2e765cf2dd4b13bc88c80e6c5fa14f
Alvaro Ferreira <alvaromachadoferreira@hotmail.com>
2022-08-19 12:12:16 -0300

    chore: update imports assessment module

--- src/modules/assessment/assessment.module.ts
+++ src/modules/assessment/assessment.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AssessmentController } from './assessment.controller';
+import { AssessmentRepository } from './assessment.repository';
 import { AssessmentService } from './assessment.service';
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([AssessmentRepository])
+    ],
   controllers: [AssessmentController],
-  providers: [AssessmentService]
+  providers: [AssessmentService],
+  exports: [AssessmentService],
 })
 export class AssessmentModule {}
