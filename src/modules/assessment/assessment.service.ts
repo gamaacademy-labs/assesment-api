5dc11f11a22524608ae76302f26541292d2b5598
Alvaro Ferreira <alvaromachadoferreira@hotmail.com>
2022-08-19 18:24:27 -0300

    feat: add return and exclude collumns logic to endpoint

--- src/modules/assessment/assessment.service.ts
+++ src/modules/assessment/assessment.service.ts
@@ -10,12 +10,26 @@ export class AssessmentService {
         private assessmentRepository: AssessmentRepository,
     ) {}
 
-    public async findAssessmentById(id: string): 
-    Promise<AssessmentEntity> {
-        const assessment = await this.assessmentRepository.
-        findAssessmentById(id);
+    public async findAssessmentById(id: string):
+    Promise<AssessmentEntity| Object>  {
+        const assessment = await this.assessmentRepository.findAssessmentById(id);
+
+
         if(!assessment) throw new NotFoundException(`Assessment with id ${id} not found`);
-        
-        return assessment;
+        const qtdQuestions:Object = assessment.questions.length;
+        const maxScore:Object = assessment.questions.length;
+
+
+
+        delete assessment.questions;
+
+        const assessmentObj = {
+            ...assessment, qtdQuestions, maxScore
+        }
+
+        return assessmentObj;
     }
+
+
+
 }
