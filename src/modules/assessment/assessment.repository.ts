0da3905a8042a1b87819958b4c8c40860467b909
Alvaro Ferreira <alvaromachadoferreira@hotmail.com>
2022-08-19 12:13:26 -0300

    fix: name class assessment repository

--- src/modules/assessment/assessment.repository.ts
+++ src/modules/assessment/assessment.repository.ts
@@ -2,7 +2,7 @@ import { EntityRepository, Repository } from "typeorm";
 import { AssessmentEntity } from "./assessment.entity";
 
 @EntityRepository(AssessmentEntity)
-export class UsersRepository extends Repository<AssessmentEntity> {
+export class AssessmentRepository extends Repository<AssessmentEntity> {
 
     public async findAssessmentById (id: string): Promise<AssessmentEntity> {
         const assessmentId = await this.findOne({
