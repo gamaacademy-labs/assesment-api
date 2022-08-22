419db69fc1651b4e15f2f8addc5621993f50d824
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-08-22 17:21:07 -0300

    refactor: add (id) in seed assessments

--- src/db/seeds/assessment.seed.ts
+++ src/db/seeds/assessment.seed.ts
@@ -9,7 +9,16 @@ export default class Assessment implements Seeder {
       .insert()
       .into(AssessmentEntity)
       .values([
-        { title: 'Avaliação de Estrutura de Dados', finishedAt: '2020-01-01', questions: [] },
+        { 
+          id: '2b1232a7-5c59-4a8c-aaf0-f6d1944c5000',
+          title: 'Avaliação de Estrutura de Dados', 
+          finishedAt: '2020-01-01', 
+          questions: [
+            'f48ac15b-0491-41d9-bf50-11a993f0306d', 
+            '5978b863-c79e-47ba-9e21-6ad5707b87cb', 
+            '4940c4cb-4301-4fe4-9ddc-e47f893a86d6'
+          ], 
+        }
       ])
       .execute();
   }
