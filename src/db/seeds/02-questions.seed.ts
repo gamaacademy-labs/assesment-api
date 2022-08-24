0b0e70cdc0d0d006ee943978f47ca55de79c6c69
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-08-24 11:25:25 -0300

    feat: add object field (questions)

--- src/db/seeds/02-questions.seed.ts
+++ src/db/seeds/02-questions.seed.ts
@@ -9,9 +9,30 @@ export default class Questions implements Seeder {
       .insert()
       .into(QuestionsEntity)
       .values([
-        { id: 'f48ac15b-0491-41d9-bf50-11a993f0306d', title: 'Title I - Question', alternatives: [] },
-        { id: '4940c4cb-4301-4fe4-9ddc-e47f893a86d6', title: 'Title II - Question', alternatives: [] },
-        { id: '5978b863-c79e-47ba-9e21-6ad5707b87cb', title: 'Title II - Question', alternatives: [] },
+        { id: 'f48ac15b-0491-41d9-bf50-11a993f0306d', title: 'Title I - Question', alternatives: [
+          { alternativeId: 1, answer: 'answer1'},
+          { alternativeId: 2, answer: 'answer2'},
+          { alternativeId: 3, answer: 'answer3'},
+          { alternativeId: 4, answer: 'answer4'},
+          { alternativeId: 5, answer: 'answer5'}
+        ]
+      },
+        { id: '4940c4cb-4301-4fe4-9ddc-e47f893a86d6', title: 'Title II - Question', alternatives: [
+          { alternativeId: 1, answer: 'answer1'},
+          { alternativeId: 2, answer: 'answer2'},
+          { alternativeId: 3, answer: 'answer3'},
+          { alternativeId: 4, answer: 'answer4'},
+          { alternativeId: 5, answer: 'answer5'}
+        ]
+      },
+        { id: '5978b863-c79e-47ba-9e21-6ad5707b87cb', title: 'Title II - Question', alternatives: [
+          { alternativeId: 1, answer: 'answer1'},
+          { alternativeId: 2, answer: 'answer2'},
+          { alternativeId: 3, answer: 'answer3'},
+          { alternativeId: 4, answer: 'answer4'},
+          { alternativeId: 5, answer: 'answer5'}
+        ] 
+      },
       ])
       .execute();
   }
