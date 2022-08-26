97eeed351fe8f8a1878d4839b15a5b05f2ab8e47
Alvaro Ferreira <alvaromachadoferreira@hotmail.com>
2022-08-26 11:43:58 -0300

    chore: import UsersRepository

--- src/modules/user-assessments/user-assessments.module.ts
+++ src/modules/user-assessments/user-assessments.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersRepository } from "../users/users.repository";
 import { UserAssessmentsController } from "./user-assessments.controller";
 import { UserAssessmentsRepository } from "./user-assessments.repository";
 import { UserAssessmentsService } from "./user-assessments.service";
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([UserAssessmentsRepository])
+        TypeOrmModule.forFeature([UserAssessmentsRepository, UsersRepository])
     ],
     controllers: [UserAssessmentsController],
     providers: [UserAssessmentsService],
