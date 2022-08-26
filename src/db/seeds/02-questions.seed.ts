677f47a74be898fe0b09f85c66b5685b1a8f7451
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-08-26 11:39:58 -0300

    refactor: updated alternatives array

--- src/db/seeds/02-questions.seed.ts
+++ src/db/seeds/02-questions.seed.ts
@@ -9,29 +9,38 @@ export default class Questions implements Seeder {
       .insert()
       .into(QuestionsEntity)
       .values([
-        { id: 'f48ac15b-0491-41d9-bf50-11a993f0306d', title: 'Title I - Question', alternatives: [
-          { alternativeId: 1, answer: 'answer1'},
-          { alternativeId: 2, answer: 'answer2'},
-          { alternativeId: 3, answer: 'answer3'},
-          { alternativeId: 4, answer: 'answer4'},
-          { alternativeId: 5, answer: 'answer5'}
-        ]
+        { id: 'f48ac15b-0491-41d9-bf50-11a993f0306d', 
+          title: 'Title I - Question', 
+          alternatives: [
+            { id: 1, title: 'alternative1' },
+            { id: 2, title: 'alternative2' },
+            { id: 3, title: 'alternative3' },
+            { id: 4, title: 'alternative4' },
+            { id: 5, title: 'alternative5' },
+        ],
+        isCorrect: [1,2]
       },
-        { id: '4940c4cb-4301-4fe4-9ddc-e47f893a86d6', title: 'Title II - Question', alternatives: [
-          { alternativeId: 1, answer: 'answer1'},
-          { alternativeId: 2, answer: 'answer2'},
-          { alternativeId: 3, answer: 'answer3'},
-          { alternativeId: 4, answer: 'answer4'},
-          { alternativeId: 5, answer: 'answer5'}
-        ]
+        { id: '4940c4cb-4301-4fe4-9ddc-e47f893a86d6', 
+          title: 'Title II - Question', 
+          alternatives: [
+            { id: 1, title: 'alternative1' },
+            { id: 2, title: 'alternative2' },
+            { id: 3, title: 'alternative3' },
+            { id: 4, title: 'alternative4' },
+            { id: 5, title: 'alternative5' }
+        ],
+        isCorrect: [1]
       },
-        { id: '5978b863-c79e-47ba-9e21-6ad5707b87cb', title: 'Title II - Question', alternatives: [
-          { alternativeId: 1, answer: 'answer1'},
-          { alternativeId: 2, answer: 'answer2'},
-          { alternativeId: 3, answer: 'answer3'},
-          { alternativeId: 4, answer: 'answer4'},
-          { alternativeId: 5, answer: 'answer5'}
-        ] 
+        { id: '5978b863-c79e-47ba-9e21-6ad5707b87cb',
+          title: 'Title II - Question', 
+          alternatives: [
+            { id: 1, title: 'alternative1' },
+            { id: 2, title: 'alternative2' },
+            { id: 3, title: 'alternative3' },
+            { id: 4, title: 'alternative4' },
+            { id: 5, title: 'alternative5' }
+        ],
+        isCorrect: [3]
       },
       ])
       .execute();
