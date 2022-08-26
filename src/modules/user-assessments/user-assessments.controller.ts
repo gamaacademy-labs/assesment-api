9a71ba0aa65b90a7a4bb836f5ecc2c0e37f3442f
Alvaro Ferreira <alvaromachadoferreira@hotmail.com>
2022-08-26 11:43:08 -0300

    refactor: endpoint name

--- src/modules/user-assessments/user-assessments.controller.ts
+++ src/modules/user-assessments/user-assessments.controller.ts
@@ -1,29 +1,22 @@
 import { Controller, Get, Param } from "@nestjs/common";
 import { ApiBearerAuth, ApiParam, ApiTags } from "@nestjs/swagger";
-import { UserAssessmentsEntity } from "./user-assessments.entity";
+import { UserAssessmentEntity } from "../assessment/user-assessment.entity";
 import { UserAssessmentsService } from "./user-assessments.service";
 
 @Controller('user-assessments')
 @ApiTags('user-assessments')
 export class UserAssessmentsController {
+    constructor(private userAssessmentsService: UserAssessmentsService) {}
 
-    constructor(
-        private usersAssessmentsService: UserAssessmentsService
-    ) { }
+    
+@Get('/:username/:id')
+@ApiBearerAuth()
+@ApiParam({ name: 'username' })
+async findUserAssessment(
+    @Param('username') username: string, 
+    @Param('id') id: string): Promise<UserAssessmentEntity | object> {
 
-    @ApiBearerAuth()
-    @Get('/:username/assessments/:id')
-    @ApiParam({
-        name: 'username',
-        name: 'id'
-    })
-    public async findUserAssessmentById(
-        @Param('username') username: string
-        @Param('id') id: string
-    ): Promise<UserAssessmentsEntity> {
-        const userAssessment =
-            await this.usersAssessmentsService.findUserAssessment(username, id)
-        return userAssessment
-    }
+    return this.userAssessmentsService.findUserAssessment(id);
+}
 
 }
