92f9b48fe98e1159214ce4fff9a0c25dd136970b
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-08-27 08:38:49 -0300

    feat: inserting previous migration

---
+++ src/db/migrations/1660748665911-add_questions.ts
@@ -0,0 +1,13 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class addQuestions1660748665911 implements MigrationInterface {
+    name = 'addQuestions1660748665911'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "questions" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT now(), "is_active" boolean NOT NULL DEFAULT true, "title" character varying NOT NULL, "alternatives" jsonb DEFAULT '[]', CONSTRAINT "PK_08a6d4b0f49ff300bf3a0ca60ac" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "questions"`);
+    }
+}
