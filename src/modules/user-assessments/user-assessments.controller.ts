fc6ccfa508532890e31edf613d980fecae1c1064
Alvaro Ferreira <alvaromachadoferreira@hotmail.com>
2022-08-31 13:08:49 -0300

    refactor: change request params to body

--- src/modules/user-assessments/user-assessments.controller.ts
+++ src/modules/user-assessments/user-assessments.controller.ts
@@ -1,24 +1,23 @@
-import { Controller, Param, Patch, Request, UseGuards } from "@nestjs/common";
+import { Body, Controller, Param, Patch, Request, UseGuards } from "@nestjs/common";
 import { ApiBearerAuth, ApiBody, ApiParam, ApiTags } from "@nestjs/swagger";
 import { JwtAuthGuard } from "../auth/strategy/jwt-auth.guard";
+import { UserAssessmentDto } from "./user-assessment.dto";
 import { UserAssessmentEntity } from "./user-assessment.entity";
 import { UserAssessmentsService } from "./user-assessments.service";
 
 @Controller('user-assessments')
 @ApiTags('user-assessments')
 export class UserAssessmentsController {
-    constructor(private userAssessmentsService: UserAssessmentsService) {}
+    constructor(private userAssessmentsService: UserAssessmentsService) { }
 
-@UseGuards(JwtAuthGuard)
-@ApiBearerAuth()
-@Patch('/questions/id/:id')
-@ApiParam({ name: 'id' })
-@ApiBody({})
-async findUserAssessment(
-    @Request() req,
-    @Param('id') assessmentId: string): Promise<UserAssessmentEntity | object> {
-    
-    return this.userAssessmentsService.registerAnswersUser(assessmentId, req.user, req.body);
-}
-
+    @UseGuards(JwtAuthGuard)
+    @ApiBearerAuth()
+    @Patch('/questions')
+    @ApiBody({ type: UserAssessmentDto })
+    async findUserAssessment(
+        @Body() payload: UserAssessmentDto,
+        @Request() req,
+    ) {
+        return this.userAssessmentsService.registerUserAnswer(payload, req.user.username);
+    }
 }
