03d15bedbd1a8a0bf4744d024486dac6f11f5658
GeFFz <gefferson_oliveira@outlook.com>
2022-08-31 02:48:22 -0300

    feat: add repositories using in service registerAnswersUser

--- src/modules/user-assessments/user-assessments.repository.ts
+++ src/modules/user-assessments/user-assessments.repository.ts
@@ -1,18 +1,48 @@
 import { EntityRepository, Repository } from "typeorm";
-import { UserAssessmentEntity } from "../assessment/user-assessment.entity";
+import { UserAssessmentEntity } from "./user-assessment.entity";
 
 
 @EntityRepository(UserAssessmentEntity)
 export class UserAssessmentsRepository extends Repository<UserAssessmentEntity> {
 
-    public async findUserAssessmentById (id: string): Promise<UserAssessmentEntity> {
-        const userAssessmentAnswers = await this.findOne({
+    public async countUserAssesmentByUsernameAndId (assessment: string, user: string): Promise<number> {
+        const userAssessmentExist = await this.count({
             where: {
-                id,
+                assessment,
+                user,
                 isActive: true
             }
         })
-        return userAssessmentAnswers
+        return userAssessmentExist
+    }
+
+    public async createUserAssessment (assessment: any, user: any): Promise<any> {
+        const newUserAssessment = await this.insert({            
+            user,
+            assessment,
+            score: 0,
+            answers: [],
+            status: true,
+        });
+        return newUserAssessment
+    }
+
+    public async findUserAssessmentById (assessment: string, user: string): Promise<UserAssessmentEntity> {
+        const newUserAssessment = await this.findOne({            
+            where: {
+                user,
+                assessment,
+                isActive: true
+            }
+        });
+        return newUserAssessment
+    }
+
+    public async updateAnswersUser (id: string, answers: []): Promise<any> {
+
+        const newUserAssessment = await this.update(id, { answers });
+        
+        return newUserAssessment
     }
 
 }
