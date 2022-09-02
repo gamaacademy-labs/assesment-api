ac8aad0f71989145c27474b516f11939185052a1
GeFFz <gefferson_oliveira@outlook.com>
2022-09-02 13:33:17 -0300

    feat: add Req to route

--- src/modules/assessment/assessment.controller.ts
+++ src/modules/assessment/assessment.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Request, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/strategy/jwt-auth.guard';
 import { AssessmentService } from './assessment.service';
@@ -13,8 +13,8 @@ export class AssessmentController {
     @UseGuards(JwtAuthGuard)
     @ApiBearerAuth()
     @Get('active')
-    public async findAssessmentsActive() {
-        return this.assessmentService.findAssessmentsActive();
+    public async findAssessmentsActive(@Request() req) {
+        return this.assessmentService.findAssessmentsActive(req.user.username);
     }
 
     @UseGuards(JwtAuthGuard)
